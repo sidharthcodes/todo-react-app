5ba3d5a11b712cc18095354bd16027ee7d9f2677
Sidharth <sidnagar08@gmail.com>
2021-05-10 14:18:13 +0530

    controlled input component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Form = () => {
+    
+    const [inputValue, setInputvalue] = useState("");
+
+    const handleInputChange = (event) => {
+        setInputvalue(event.target.value);
+    }
+
     return(
         <form className="ui form">
             <div className="ui grid center aligned">
                 <div className="row">
                     <div className="column six wide">
-                        <input type="text" placeholder="Enter task todo..."/>
+                        <input
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        type="text"
+                        placeholder="Enter task todo..."/>
                     </div>
                     <div className="column one wide">
                         <button type="submit" className="ui button circular icon green"><i className="plus icon"></i></button>
