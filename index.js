2b830bfd834cfc51c7c41daf20150cc8afe2c64a
Sidharth <sidnagar08@gmail.com>
2021-05-12 20:26:12 +0530

    fixes

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 const todoRouter = require("./routes/todoRouter");
+
+
+mongoose.connect("mongodb://localhost/todolist")
+    .then(() => { console.log("Connect to DB successfully") })
+    .catch((err) => console.error(err));
 
 app.use("/todo", todoRouter);
 
--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -1,12 +1,7 @@
 const router = require("express").Router();
-const mongoose = require("mongoose");
 
 router.get("/", (req, res) => {
     console.log("Index Page");
 });
-
-mongoose.connect("mongodb://localhost/todolist")
-    .then(() => { console.log("Connect to DB successfully") })
-    .catch((err) => console.error(err));
 
 module.exports = router;
