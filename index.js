f6ca3ae4b06f533f91d7e4825c10b4ffdbd93bee
Sidharth <sidnagar08@gmail.com>
2021-05-13 16:50:36 +0530

    cors issue solved !

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 const todoRouter = require("./routes/todoRouter");
 const connectOptions = { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false };
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb://localhost/todolist", connectOptions)
-    .then(() => console.log("Connected successfully"))
+    .then(() => console.log("Connected successfully to DB"))
     .catch((err) => console.error(err));
 
 app.use("/todos", todoRouter);
